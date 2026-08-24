8a551aeff97f9cf96d2ada9497d2f881b04675a9
Thomas Renaud <thomasre@bam.tech>
2026-08-24 16:43:08 +0200

    refactor: use absolute fill instead of flex at index

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,10 @@ import { StyleSheet, View } from "react-native";
 
 const Fishes = () => {
   return (
-    <View style={styles.container}>
+    <View style={StyleSheet.absoluteFill}>
       <Nemo />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default Fishes;
