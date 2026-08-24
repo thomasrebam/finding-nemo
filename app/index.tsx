6197881455ab8e16c52f66b7261ff5db16e5e263
Thomas Renaud <thomasre@bam.tech>
2026-08-24 10:23:19 +0200

    refactor: extract Nemo in its own file

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,10 @@
-import { Body } from "@/components/animation/Body/Body";
+import { Nemo } from "@/components/animation/Nemo";
 import { StyleSheet, View } from "react-native";
 
 const Fishes = () => {
   return (
     <View style={styles.container}>
-      <Body
-        animal={{
-          spine: [
-            { x: 15, y: 15, size: 10, displayedSize: 10 },
-            { x: 30, y: 30, size: 12, displayedSize: 12 },
-            { x: 45, y: 45, size: 10, displayedSize: 10 },
-            { x: 60, y: 60, size: 9, displayedSize: 9 },
-            { x: 75, y: 75, size: 8, displayedSize: 8 },
-            { x: 90, y: 90, size: 7, displayedSize: 7 },
-            { x: 105, y: 105, size: 7, displayedSize: 6 },
-            { x: 120, y: 120, size: 7, displayedSize: 5 },
-            { x: 135, y: 135, size: 7, displayedSize: 4 },
-            { x: 150, y: 150, size: 7, displayedSize: 3 },
-            { x: 165, y: 165, size: 7, displayedSize: 2 },
-            { x: 165, y: 165, size: 7, displayedSize: 2 },
-            { x: 180, y: 180, size: 7, displayedSize: 1 },
-            { x: 180, y: 180, size: 7, displayedSize: 1 },
-            { x: 180, y: 180, size: 7, displayedSize: 1 },
-          ],
-        }}
-      />
+      <Nemo />
     </View>
   );
 };
