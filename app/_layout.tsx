2a09f55f4cae0c38795828fae7d1d6a9942620aa
Thomas Renaud <thomasre@bam.tech>
2026-09-02 20:05:21 +0200

    feat: hide header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.container}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </GestureHandlerRootView>
   );
 }
