eaf398a4ef0ced6cb67dae2ace78b749f0a986bb
Thomas Renaud <thomasre@bam.tech>
2026-09-02 22:17:01 +0200

    refactor: remove unuseful comment

--- components/animation/animation.ts
+++ components/animation/animation.ts
@@ -25,7 +25,6 @@ export const moveAnimalTo = ({
   const oldSpine = [...animal.spine];
   const newSpine = [...animal.spine];
 
-  // Calculate positions for other nodes
   for (let index = 0; index < oldSpine.length; index++) {
     const currentNode = oldSpine[index];
 
