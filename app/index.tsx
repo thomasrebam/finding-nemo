8c4410085e65f1912e62f443a9d6340a83dcb3fb
Thomas Renaud <thomasre@bam.tech>
2026-09-10 20:48:38 +0200

    feat: remove safe area view for the edge to edge

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,11 @@
 import { Nemo } from "@/components/animation/Nemo";
 import { View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Fishes = () => {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-      }}
-      edges={["top", "bottom"]}
-    >
-      <View style={{ flex: 1 }}>
-        <Nemo />
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <Nemo />
+    </View>
   );
 };
 
