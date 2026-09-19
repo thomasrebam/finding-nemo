7fec2992727b7f20d43da03fb3298b4f7bb6b4e4
Thomas Renaud <thomasre@bam.tech>
2026-09-19 15:36:39 +0200

    feat: add gradient to skia fish body

--- components/animation/Body/SkiaFishBody.tsx
+++ components/animation/Body/SkiaFishBody.tsx
@@ -1,5 +1,12 @@
 import { NemoColors } from "@/constants/Colors";
-import { Canvas, Circle, Group, Path } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Circle,
+  Group,
+  LinearGradient,
+  Path,
+  vec,
+} from "@shopify/react-native-skia";
 import { StyleSheet, View } from "react-native";
 import { SharedValue, useDerivedValue } from "react-native-reanimated";
 import { AnimalNode } from "../animation";
@@ -11,6 +18,14 @@ export const SkiaFishBody = ({
   spine: SharedValue<AnimalNode[]>;
 }) => {
   const { fishPath } = useFishPath(spine);
+  const gradientStart = useDerivedValue(() => {
+    const head = spine.value[0];
+    return vec(head.x, head.y);
+  }, [spine]);
+  const gradientEnd = useDerivedValue(() => {
+    const tail = spine.value[spine.value.length - 1];
+    return vec(tail.x, tail.y);
+  }, [spine]);
 
   // Create separate derived values for each eye coordinate
   const eyes = useEyesValues(spine);
@@ -25,12 +40,26 @@ export const SkiaFishBody = ({
       >
         <Group>
           {/* Fish body */}
-          <Path
-            path={fishPath}
-            color={NemoColors.orange}
-            style="fill"
-            opacity={1}
-          />
+          <Path path={fishPath} style="fill" opacity={1}>
+            <LinearGradient
+              start={gradientStart}
+              end={gradientEnd}
+              colors={[
+                NemoColors.orange,
+                NemoColors.white,
+                NemoColors.orange,
+                NemoColors.white,
+                NemoColors.orange,
+                NemoColors.white,
+                NemoColors.orange,
+                NemoColors.white,
+                NemoColors.orange,
+                NemoColors.white,
+                NemoColors.orange,
+                NemoColors.white,
+              ]}
+            />
+          </Path>
 
           {/* Eyes */}
           <Circle
